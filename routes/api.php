a8a3810c59b55f34664610b98594348e150d1d74
Mohammad Ali Khan <xvirus.bd@gmail.com>
2023-04-24 00:12:18 +0600

    reset password api

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\Admin\ProductDetailsController;
 use App\Http\Controllers\Admin\NotificationController;
 use App\Http\Controllers\User\AuthController;
 use App\Http\Controllers\User\ForgetController;
+use App\Http\Controllers\User\ResetController;
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -29,6 +30,8 @@ Route::post('/login', [AuthController::class, 'Login']);
 Route::post('/register', [AuthController::class, 'Register']);
 // Forget Password Routes 
 Route::post('/forgetpassword', [ForgetController::class, 'ForgetPassword']);
+// Reset Password Routes 
+Route::post('/resetpassword', [ResetController::class, 'ResetPassword']);
 
 
 //Get Visitor 
